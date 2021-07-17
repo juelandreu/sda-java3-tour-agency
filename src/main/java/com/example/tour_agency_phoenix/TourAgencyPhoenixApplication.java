61e2c60c403d47a75787ee648850f187c77516af
Aleksia <aleksiabega123@gmail.com>
2021-07-17 11:41:33 +0200

    To Spring Boot

---
+++ src/main/java/com/example/tour_agency_phoenix/TourAgencyPhoenixApplication.java
@@ -0,0 +1,13 @@
+package com.example.tour_agency_phoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgencyPhoenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourAgencyPhoenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tour_agency_phoenix/TourAgencyPhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tour_agency_phoenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourAgencyPhoenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
