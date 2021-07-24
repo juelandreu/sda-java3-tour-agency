13926a4a0fc1d099c11837e1c10c829a9758d2ce
User <erjonzeqiri9@gmail.com>
2021-07-24 14:59:10 +0200

    success page

---
+++ src/main/java/com/example/tour_agency_phoenix/controllers/SuccessController.java
@@ -0,0 +1,13 @@
+package com.example.tour_agency_phoenix.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class SuccessController {
+    @GetMapping("/success")
+    public String payment(){
+        return "success";
+    }
+}
