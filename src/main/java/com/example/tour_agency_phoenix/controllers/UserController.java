3aad02394b75eb45f95272a18ce7728aeb0a94b9
Aleksia <aleksiabega123@gmail.com>
2021-08-01 10:50:43 +0200

    save

--- src/main/java/com/example/tour_agency_phoenix/controllers/UserController.java
+++ src/main/java/com/example/tour_agency_phoenix/controllers/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -30,6 +27,11 @@ public class UserController {
         var  item = this.userService.findById(id);
         return item.isPresent() ? ResponseEntity.ok(item.get())
                 :new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public UUID save(@RequestBody User user) {
+        return userService.save(user);
     }
 
     @GetMapping
